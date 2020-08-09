06e6b1f60ec61559f831f2fe543ee9d2d337b8d6
justinparrish7@gmail.com <justinparrish7@gmail.com>
2020-08-09 15:17:29 -0400

    updating files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import SideNavigation from './Components/Pages/SideNavigation/SideNavigation'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+    <Container>
+      <Row>
+        <Col sm={4}>
+          <Button>Click Here!</Button>
+        </Col>
+        <Col sm={8}>
+          <button>Click Here!</button>
+        </Col>
+      </Row>
+    </Container>
+  )
 
 export default App;
---
+++ src/Components/Pages/SideNavigation/SideNavigation.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Nav } from 'react-bootstrap'
+
+const SideNavigation = () => (
+    <Nav>
+        <Nav.Item>
+            
+        </Nav.Item>
+    </Nav>
+)
+
+export default SideNavigation
