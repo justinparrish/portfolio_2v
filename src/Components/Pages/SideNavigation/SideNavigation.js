61eb084d5eb5fb0eb414864b25e1ef4e0570f187
justinparrish7@gmail.com <justinparrish7@gmail.com>
2021-02-21 14:22:32 -0500

    Courtesy commit

--- src/Components/Pages/SideNavigation/SideNavigation.js
+++ src/Components/Pages/SideNavigation/SideNavigation.js
@@ -3,12 +3,13 @@ import { Nav } from 'react-bootstrap'
 
 const SideNavigation = () => (
     <Nav defaultActiveKey="/home" className="flex-column">
-  <Nav.Link ><span className="navLinkText">Home</span></Nav.Link>
-  <Nav.Link ><span className="navLinkText">About</span></Nav.Link>
-  <Nav.Link ><span className="navLinkText">Experience</span></Nav.Link>
-  <Nav.Link ><span className="navLinkText">Work</span></Nav.Link>
-  <Nav.Link ><span className="navLinkText">Contact</span></Nav.Link>
-</Nav>
+        <hr />
+        <Nav.Link ><span className="navLinkText">Home</span></Nav.Link>
+        <Nav.Link ><span className="navLinkText">About</span></Nav.Link>
+        <Nav.Link ><span className="navLinkText">Experience</span></Nav.Link>
+        <Nav.Link ><span className="navLinkText">Work</span></Nav.Link>
+        <Nav.Link ><span className="navLinkText">Contact</span></Nav.Link>
+    </Nav>
 )
 
 export default SideNavigation
